b262a2c5cdd90ba30bf742641de5c8bd985fcd4d
EstebanCaponio <estebancaponio@gmail.com>
2025-08-20 15:08:55 +0200

    feat: milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,40 @@
-// import { useEffect, useState } from "react"
+const debounce = (callback, delay) => {
+  let timeout;
+  return (value) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      callback(value)
+    }, delay);
+  };
+};
 
-// function App() {
-
-
-//   const [query, setQuery] = useState('');
-//   const [suggestions, setSuggestions] = useState([]);
-//   console.log(suggestions)
-
-//   useEffect(() => {
-//     if (!query.trim()) {
-//       setSuggestions([]);
-//       return
-//     }
-//     fetch(`http://localhost:3333/products?search=${query}`)
-//       .then(res => res.json())
-//       .then(data => setSuggestions(data))
-//       .catch(e => console.error(e));
-//   }, [query]);
-
-
-
-//   return (
-//     <>
-//       <div>
-//         <input type="text"
-//           placeholder="Search..."
-//           value={query}
-//           onChange={e => setQuery(e.target.value)}
-//         />
-//         {suggestions.length > 0 && (
-//           <div>
-//             {suggestions.map((product) => (
-//               <p key={product.id}>{product.name}</p>
-//             ))}
-//           </div>
-//         )}
-//       </div>
-//     </>
-//   )
-// }
-
-// export default App
-
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
   const [query, setQuery] = useState('');
   const [suggestions, setSuggestions] = useState([]);
 
-  useEffect(() => {
+  const fetchProducts = async (query) => {
     if (!query.trim()) {
       setSuggestions([]);
       return;
     }
+    try {
+      const res = await fetch(`http://localhost:3333/products?search=${query}`);
+      const data = await res.json();
+      setSuggestions(data);
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-    fetch(`http://localhost:3333/products?search=${query}`)
-      .then(res => {
-        if (!res.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return res.json();
-      })
-      .then(data => setSuggestions(data))
-      .catch(e => console.error("Errore nel fetch dei dati:", e));
+
+  const debouncedFetchProducts = useCallback(
+    debounce(fetchProducts, 500), []
+  );
+
+  useEffect(() => {
+    debouncedFetchProducts(query);
   }, [query]);
 
   return (
